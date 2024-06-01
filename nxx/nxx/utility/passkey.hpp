1496483ff7523a16286f07986868578f35d6980a
3uclid3 <3uclid3@pm.me>
2024-05-31 20:32:00 -0400

    Refactor pass_key class to passkey

--- nxx/nxx/utility/passkey.hpp
+++ nxx/nxx/utility/passkey.hpp
@@ -3,13 +3,13 @@
 namespace nxx {
 
 template<typename T>
-class pass_key
+class passkey
 {
 private:
     friend T;
 
     // Avoid = default to disallow creation by uniform initialization.
-    pass_key()
+    passkey()
     {}
 };
 
