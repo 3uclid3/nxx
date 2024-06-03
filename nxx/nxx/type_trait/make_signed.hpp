30f1c7f6bd4848fbde238cc778e714ced6d867b8
3uclid3 <3uclid3@pm.me>
2024-06-02 21:56:01 -0400

    Add type_trait functions for signed and unsigned types
    - Added functions to handle copying const and volatile qualifiers for types
    - Implemented make_signed and make_unsigned functions for integral and enum types

--- nxx/nxx/type_trait/make_signed.hpp
+++ nxx/nxx/type_trait/make_signed.hpp
@@ -1,8 +1,33 @@
 #pragma once
 
+#include <nxx/type_trait/is_enum.hpp>
+#include <nxx/type_trait/is_integral.hpp>
+#include <nxx/type_trait/copy_const_volatile.hpp>
+#include <nxx/type_trait/remove_const_volatile.hpp>
+
 namespace nxx {
 
-template<typename T>
-using make_signed = __make_signed(T);
+namespace details {
+
+template<typename T, bool = is_integral<T> || is_enum<T>>
+struct make_signed_impl
+{};
+
+// clang-format off
+template <> struct make_signed_impl<bool, true> {};
+template <> struct make_signed_impl<s8_t, true> { using type = s8_t; };
+template <> struct make_signed_impl<u8_t, true> { using type = s8_t; };
+template <> struct make_signed_impl<s16_t, true> { using type = s16_t; };
+template <> struct make_signed_impl<u16_t, true> { using type = s16_t; };
+template <> struct make_signed_impl<s32_t, true> { using type = s32_t; };
+template <> struct make_signed_impl<u32_t, true> { using type = s32_t; };
+template <> struct make_signed_impl<s64_t, true> { using type = s64_t; };
+template <> struct make_signed_impl<u64_t, true> { using type = s64_t; };
+// clang-format on
+
+} // namespace details
+
+template<class T>
+using make_signed = copy_const_volatile<T, typename details::make_signed_impl<remove_const_volatile<T>>::type>;
 
 } // namespace nxx
--- nxx/nxx/type_trait/make_unsigned.hpp
+++ nxx/nxx/type_trait/make_unsigned.hpp
@@ -1,8 +1,33 @@
 #pragma once
 
+#include <nxx/type_trait/is_enum.hpp>
+#include <nxx/type_trait/is_integral.hpp>
+#include <nxx/type_trait/copy_const_volatile.hpp>
+#include <nxx/type_trait/remove_const_volatile.hpp>
+
 namespace nxx {
 
-template<typename T>
-using make_unsigned = __make_unsigned(T);
+namespace details {
+
+template<typename T, bool = is_integral<T> || is_enum<T>>
+struct make_unsigned_impl
+{};
+
+// clang-format off
+template <> struct make_unsigned_impl<bool, true> {};
+template <> struct make_unsigned_impl<s8_t, true> { using type = u8_t; };
+template <> struct make_unsigned_impl<u8_t, true> { using type = u8_t; };
+template <> struct make_unsigned_impl<s16_t, true> { using type = u16_t; };
+template <> struct make_unsigned_impl<u16_t, true> { using type = u16_t; };
+template <> struct make_unsigned_impl<s32_t, true> { using type = u32_t; };
+template <> struct make_unsigned_impl<u32_t, true> { using type = u32_t; };
+template <> struct make_unsigned_impl<s64_t, true> { using type = u64_t; };
+template <> struct make_unsigned_impl<u64_t, true> { using type = u64_t; };
+// clang-format on
+
+} // namespace details
+
+template<class T>
+using make_unsigned = copy_const_volatile<T, typename details::make_unsigned_impl<remove_const_volatile<T>>::type>;
 
 } // namespace nxx
