9223435726cc6ced837b07b61685b6176b82f047
3uclid3 <3uclid3@pm.me>
2024-06-07 21:42:20 -0400

    Added expand_allocate and reallocator functions

--- nxx/nxx/memory/allocate.hpp
+++ nxx/nxx/memory/allocate.hpp
@@ -5,7 +5,9 @@
 
 namespace nxx {
 
-memory_block allocate(size_t size);
+[[nodiscard]] memory_block allocate(size_t size);
+bool expand_allocate(memory_block& block, size_t delta);
+bool reallocate(memory_block& block, size_t new_size);
 void deallocate(memory_block& block);
 
 } // namespace nxx
